c3c8f5a9187c41d3cfb001f20ea70d5a44eb2d6d
Dhairya211206 <dhairya.dabi@gmail.com>
2026-02-04 21:29:08 +0530

    Added login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/Register';
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
